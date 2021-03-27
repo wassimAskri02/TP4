b3e4a554cc27338efedffbc478ef0e873c91c270
pub <pub@samsung>
2021-03-27 21:58:48 +0100

    Authentification project

---
+++ TP4/src/isetj/servlets/Authentification.java
@@ -0,0 +1,52 @@
+package isetj.servlets;
+
+import java.io.*;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Authentification
+ */
+@WebServlet("/Authentification")
+public class Authentification extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Authentification() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		String username = request.getParameter("username");
+		String passworld = request.getParameter("passworld");
+		String dsi;
+		PrintWriter out = response.getWriter();
+		if (username.equals("dsi")) {
+				out.println("correct");
+		} else {
+			out.println("NoN Correct");
+		}
+        
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+		
+	}
+
+}
